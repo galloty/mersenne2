25fd545b36d327d5e5e7af32d9dac346fe6b40c5
galloty <galloty@orange.fr>
2026-05-07 12:20:17 +0200

    a bit of algebra

--- design/mul_2nx3_twisted.cpp
+++ design/mul_2nx3_twisted.cpp
@@ -13,20 +13,20 @@ Please give feedback to the authors if improvement is realized. It is distribute
 class GF
 {
 private:
-	uint64_t n;
+	uint64_t _n;
 
 	static const uint64_t _p = 3 * (uint64_t(1) << 41) + 1;
 	static const uint64_t _primroot = 5;
 
 public:
 	GF() {}
-	GF(const uint64_t l) : n(l) {}
+	GF(const uint64_t n) : _n(n) {}
 
-	uint64_t get() const { return n; }
+	uint64_t get() const { return _n; }
 
-	GF & operator+=(const GF & rhs) { const uint64_t c = (n >= _p - rhs.n) ? _p : 0; n += rhs.n; n -= c; return *this; }
-	GF & operator-=(const GF & rhs) { const uint64_t c = (n < rhs.n) ? _p : 0; n -= rhs.n; n += c; return *this; }
-	GF & operator*=(const GF & rhs) { *this = uint64_t((n * __uint128_t(rhs.n)) % _p); return *this; }
+	GF & operator+=(const GF & rhs) { const uint64_t c = (_n >= _p - rhs._n) ? _p : 0; _n += rhs._n; _n -= c; return *this; }
+	GF & operator-=(const GF & rhs) { const uint64_t c = (_n < rhs._n) ? _p : 0; _n -= rhs._n; _n += c; return *this; }
+	GF & operator*=(const GF & rhs) { *this = uint64_t((_n * __uint128_t(rhs._n)) % _p); return *this; }
 
 	GF operator+(const GF & rhs) const { GF r = *this; r += rhs; return r; }
 	GF operator-(const GF & rhs) const { GF r = *this; r -= rhs; return r; }
@@ -52,58 +52,84 @@ public:
 	static const GF root_nth(const size_t n) { return GF(_primroot).pow((_p - 1) / n); }
 };
 
-static void square3(GF * const P)
+static const GF J = GF::root_nth(3), inv3 = GF(3).invert();
+
+// a quadratic polynomial modulo x^3 - 1 over the finite field
+class Quad
 {
-	static const GF J = GF::root_nth(3), J2 = J * J, inv3 = GF(3).invert();
+private:
+	GF _x[3];
 
-	const GF u0 = P[0], u1 = P[1], u2 = P[2];
-	GF v0 = (u0 +       u1 +      u2);
-	GF v1 = (u0 +   J * u1 + J2 * u2);
-	GF v2 = (u0 +  J2 * u1 +  J * u2);
-	v0 *= v0; v1 *= v1; v2 *= v2;
-	P[0] = (v0 +      v1 +      v2) * inv3;
-	P[1] = (v0 + J2 * v1 +  J * v2) * inv3;
-	P[2] = (v0 +  J * v1 + J2 * v2) * inv3;
+public:
+	Quad() {}
+	Quad(const GF x[3]) { for (size_t i = 0; i < 3; ++i) _x[i] = x[i]; }
 
-	// const GF c = P[0], b = P[1], a = P[2];
-	// P[0] = (a * b) * 2 + c * c;
-	// P[1] = (b * c) * 2 + a * a;
-	// P[2] = (c * a) * 2 + b * b;
+	void get(GF x[3]) const { for (size_t i = 0; i < 3; ++i) x[i] = _x[i]; }
+
+	Quad & operator+=(const Quad & rhs) { for (size_t i = 0; i < 3; ++i) _x[i] += rhs._x[i]; return *this; }
+	Quad & operator-=(const Quad & rhs) { for (size_t i = 0; i < 3; ++i) _x[i] -= rhs._x[i]; return *this; }
+	Quad & operator*=(const GF & rhs) { for (size_t i = 0; i < 3; ++i) _x[i] *= rhs; return *this; }
+
+	Quad operator+(const Quad & rhs) const { Quad r = *this; r += rhs; return r; }
+	Quad operator-(const Quad & rhs) const { Quad r = *this; r -= rhs; return r; }
+	Quad operator*(const GF & rhs) const { Quad r = *this; r *= rhs; return r; }
+
+	Quad dot(const Quad & rhs) const { Quad r = *this; for (size_t i = 0; i < 3; ++i) r._x[i] *= rhs._x[i]; return r; }
+
+	void forward3()
+	{
+		const GF u0 = _x[0], u1 = _x[1], u2 = _x[2];
+		const GF t = J * (u1 - u2);
+		_x[0] = u0 + u1 + u2;
+		_x[1] = u0 - u2 + t;
+		_x[2] = u0 - u1 - t;
+	}
+
+	void backward3()
+	{
+		const GF u0 = _x[0], u1 = _x[1], u2 = _x[2];
+		const GF t = J * (u1 - u2);
+		_x[0] = u0 + u1 + u2;
+		_x[1] = u0 - u1 - t;
+		_x[2] = u0 - u2 + t;
+		*this *= inv3;
+	}
+};
+
+inline Quad pow_w(const GF & w, const size_t i)
+{
+	GF wi[3]; wi[0] = w.pow(3 * i + 0); wi[1] = wi[0] * w; wi[2] = wi[1] * w;
+	return Quad(wi);
 }
 
-static void forward2(GF * const z, const size_t m, const size_t s)
+static void forward2(Quad * const q, const size_t m, const size_t s)
 {
-	const GF wm = GF::root_nth(2 * m);
+	const GF wm = GF::root_nth(6 * m);
 
 	for (size_t j = 0; j < s; ++j)
 	{
 		for (size_t i = 0; i < m; ++i)
 		{
 			const size_t k = 2 * m * j + i;
-			const GF u0 = z[k + 0 * m], u1 = z[k + 1 * m];
-			z[k + 0 * m] = u0 + u1; z[k + 1 * m] = (u0 - u1) * wm.pow(i);;
+			const Quad u0 = q[k + 0 * m], u1 = q[k + 1 * m];
+			q[k + 0 * m] = u0 + u1; q[k + 1 * m] = (u0 - u1).dot(pow_w(wm, i));
 		}
 	}
 }
 
-static void backward2(GF * const z, const size_t m, const size_t s)
+static void backward2(Quad * const q, const size_t m, const size_t s)
 {
-	const GF wmi = GF::root_nth(2 * m).invert(), inv2 = GF(2).invert();
+	const GF wmi = GF::root_nth(6 * m).invert(), inv2 = GF(2).invert();
 
 	for (size_t j = 0; j < s; ++j)
 	{
 		for (size_t i = 0; i < m; ++i)
 		{
 			const size_t k = 2 * m * j + i;
-			const GF u0 = z[k + 0 * m], u1 = z[k + 1 * m] * wmi.pow(i);
-			z[k + 0 * m] = (u0 + u1) * inv2; z[k + 1 * m] = (u0 - u1) * inv2;
+			const Quad u0 = q[k + 0 * m], u1 = q[k + 1 * m].dot(pow_w(wmi, i));
+			q[k + 0 * m] = (u0 + u1) * inv2; q[k + 1 * m] = (u0 - u1) * inv2;
 		}
 	}
-}
-
-static void sqr(GF * const z, const size_t m)
-{
-	for (size_t i = 0; i < m; ++i) square3(&z[3 * i]);
 }
 
 // Q = P^2 mod x^n - 1
@@ -137,13 +163,24 @@ int main()
 	const size_t n = 3 << 12;
 	GF P[n]; for (size_t i = 0; i < n; ++i) P[i] = GF(uint64_t(std::rand()) % 1000u);
 
-	GF Q[n]; square_slow(Q, P, n);
+	GF R[n]; square_slow(R, P, n);
 
-	for (size_t m = n / 2, s = 1; m >= 3; m /= 2, s *= 2) forward2(P, m, s);
-	sqr(P, n / 3);
-	for (size_t m = 3, s = n / 6; s >= 1; m *= 2, s /= 2) backward2(P, m, s);
+	Quad Q[n / 3];
+	for (size_t i = 0; i < n / 3; ++i) Q[i] = Quad(&P[3 * i]);
 
-	check(P, Q, n);
+	for (size_t m = n / 6, s = 1; m >= 1; m /= 2, s *= 2) forward2(Q, m, s);
+	for (size_t i = 0; i < n / 3; ++i)
+	{
+		Quad & Qi = Q[i];
+		Qi.forward3();
+		Qi = Qi.dot(Qi);
+		Qi.backward3();
+	}
+	for (size_t m = 1, s = n / 6; s >= 1; m *= 2, s /= 2) backward2(Q, m, s);
+
+	for (size_t i = 0; i < n / 3; ++i) Q[i].get(&P[3 * i]);
+
+	check(P, R, n);
 
 	return EXIT_SUCCESS;
 }
