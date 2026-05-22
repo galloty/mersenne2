4d085e989d8728f557938aeb8929fe9b103f0379
galloty <galloty@orange.fr>
2026-05-22 17:18:21 +0200

    radix-4

--- mersenne2.cpp
+++ mersenne2.cpp
@@ -9,6 +9,9 @@ Please give feedback to the authors if improvement is realized. It is distribute
 #include <iostream>
 #include <array>
 
+inline uint32_t mul_hi(const uint32_t x, const uint32_t y) { return uint32_t((x * uint64_t(y)) >> 32); }
+inline uint32_t div3(const uint32_t n) { return mul_hi(n, 1431655766u); }	// = n / 3 if n < 2^31
+
 // Z/M_qZ: the prime field of order M_q = 2^q - 1
 // q_inv = 1 (mod q), transform_size | q
 template<int q, uint32_t primroot, uint64_t q_inv, typename uint_t, typename ulong_t>
@@ -32,8 +35,7 @@ public:
 
 	bool operator!=(const ZMq & rhs) const { return (_n != rhs._n); }
 
-	// ZMq neg() const { return ZMq((_n == 0) ? 0 : _p - _n); }
-	// ZMq half() const { return ZMq(((_n % 2 == 0) ? _n : (_n + _p)) / 2); }
+	ZMq neg() const { return ZMq((_n == 0) ? 0 : _p - _n); }
 
 	ZMq & operator*=(const ZMq & rhs) { _n = _mul(_n, rhs._n); return *this; }
 
@@ -107,11 +109,11 @@ public:
 	{
 		size_t r = n; uint8_t ln = 1; while (r % 2 == 0) { r /= 2; ++ln; }
 
-		if (r == 1)
+		if (r == 1)	// n = 2^m
 		{
 			for (size_t k = 0; k < n; ++k) x[k] = x[k].rshift(w_ib[k] + ln);
 		}
-		else	// n = 3 * 2^m
+		else		// n = 3 * 2^m
 		{
 			static const ZMq inv6 = ZMq(6).invert();
 			for (size_t k = 0; k < n; ++k) x[k] = x[k].rshift(w_ib[k] + ln) * inv6;
@@ -133,9 +135,9 @@ public:
 
 	void store(Zp & s0, Zp & s1) const { s0 = _s0; s1 = _s1; }
 
-	// GFp2 conj() const { return GFp2(_s0, _s1.neg()); }
+private:
+	GFp2 conj() const { return GFp2(_s0, _s1.neg()); }
 	// GFp2 muli() const { return GFp2(_s1.neg(), _s0); }
-	// GFp2 half() const { return GFp2(_s0.half(), _s1.half()); }
 
 	GFp2 operator+(const GFp2 & rhs) const { return GFp2(_s0 + rhs._s0, _s1 + rhs._s1); }
 	GFp2 operator-(const GFp2 & rhs) const { return GFp2(_s0 - rhs._s0, _s1 - rhs._s1); }
@@ -155,63 +157,148 @@ public:
 	GFp2 mulconj(const GFp2 & rhs) const { return GFp2(_s0 * rhs._s0 + _s1 * rhs._s1, _s1 * rhs._s0 - _s0 * rhs._s1); }
 	GFp2 mul_neg0(const GFp2 & rhs) const { return GFp2(_s1 * rhs._s1 - _s0 * rhs._s0, _s1 * rhs._s0 + _s0 * rhs._s1); }
 
-	GFp2 pow(const __uint128_t e) const
+	// Radix-2 DIF butterfly
+	static void fwd2(Zp * const x0, Zp * const x1, const GFp2 & w, const size_t step)
 	{
-		if (e == 0) return GFp2(Zp(1), Zp(0));
-		GFp2 r = GFp2(Zp(1), Zp(0)), y = *this;
-		for (__uint128_t i = e; i != 1; i /= 2) { if (i % 2 != 0) r = r.mul(y); y = y.sqr(); }
-		return r.mul(y);
+		const GFp2 z0 = GFp2(x0[0], x0[step]), z1 = GFp2(x1[0], x1[step]);
+		const GFp2 u0 = z0 + z1, u1 = (z0 - z1).mul(w);
+		u0.store(x0[0], x0[step]); u1.store(x1[0], x1[step]);
 	}
 
-	GFp2 invert() const { const __uint128_t t = Zp::get_p(); return pow(t * t - 2); }
+	// Radix-2 DIT butterfly
+	static void bck2(Zp * const x0, Zp * const x1, const GFp2 & w, const size_t step)
+	{
+		const GFp2 z0 = GFp2(x0[0], x0[step]), z1 = GFp2(x1[0], x1[step]).mulconj(w);
+		const GFp2 u0 = z0 + z1, u1 = z0 - z1;
+		u0.store(x0[0], x0[step]); u1.store(x1[0], x1[step]);
+	}
 
-	static const GFp2 root_nth(const size_t n) { return GFp2(Zp(primroot_0), Zp(primroot_1)).pow(primroot_order / n); }
+	// Radix-4 DIF butterfly
+	static void fwd4(Zp * const x0, Zp * const x1, Zp * const x2, Zp * const x3, const GFp2 & w1, const GFp2 & w2, const GFp2 & w3, const size_t step)
+	{
+		const GFp2 z0 = GFp2(x0[0], x0[step]), z1 = GFp2(x1[0], x1[step]), z2 = GFp2(x2[0], x2[step]), z3 = GFp2(x3[0], x3[step]);
+		const GFp2 u0 = z0 + z2, u2 = z0 - z2, u1 = z1 + z3, u3 = z1 - z3;
+		const GFp2 v0 = u0 + u1, v1 = (u0 - u1).mul(w2), v2 = u2.addi(u3).mul(w1), v3 = u2.subi(u3).mul(w3);
+		v0.store(x0[0], x0[step]); v1.store(x1[0], x1[step]); v2.store(x2[0], x2[step]); v3.store(x3[0], x3[step]);
+	}
 
-	// Radix-2 DIF
-	template<size_t L>
+	// Radix-4 DIT butterfly
+	static void bck4(Zp * const x0, Zp * const x1, Zp * const x2, Zp * const x3, const GFp2 & w1, const GFp2 & w2, const GFp2 & w3, const size_t step)
+	{
+		const GFp2 z0 = GFp2(x0[0], x0[step]), z1 = GFp2(x1[0], x1[step]).mulconj(w2), z2 = GFp2(x2[0], x2[step]).mulconj(w1), z3 = GFp2(x3[0], x3[step]).mulconj(w3);
+		const GFp2 u0 = z0 + z1, u1 = z0 - z1, u2 = z2 + z3, u3 = z2 - z3;
+		const GFp2 v0 = u0 + u2, v2 = u0 - u2, v1 = u1.subi(u3), v3 = u1.addi(u3);
+		v0.store(x0[0], x0[step]); v2.store(x2[0], x2[step]); v1.store(x1[0], x1[step]); v3.store(x3[0], x3[step]);
+	}
+
+	// Radix-2 DIF, one iteration
 	static void forward2(Zp * const x, const GFp2 * const w, const size_t m, const size_t s)
 	{
 		for (size_t j = 0; j < s; ++j)
 		{
 			for (size_t i = 0; i < m; ++i)
 			{
-				const size_t k = L * (2 * m * j + i);
-				const GFp2 w_is = w[i * s];
-
-				for (size_t l = 0; l < L; ++l)
-				{
-					const GFp2 u0 = GFp2(x[2 * (k + 0 * m) + 0 + l], x[2 * (k + 0 * m) + L + l]);
-					const GFp2 u1 = GFp2(x[2 * (k + L * m) + 0 + l], x[2 * (k + L * m) + L + l]);
-					const GFp2 v0 = u0 + u1, v1 = (u0 - u1).mul(w_is);
-					v0.store(x[2 * (k + 0 * m) + 0 + l], x[2 * (k + 0 * m) + L + l]);
-					v1.store(x[2 * (k + L * m) + 0 + l], x[2 * (k + L * m) + L + l]);
-				}
+				const size_t k = 4 * m * j + 2 * i;
+				fwd2(&x[k + 0 * m], &x[k + 2 * m], w[i * s], 1);
 			}
 		}
 	}
 
-	// Radix-2 DIT
-	template<size_t L>
+	// Radix-2 DIT, one iteration
 	static void backward2(Zp * const x, const GFp2 * const w, const size_t m, const size_t s)
 	{
 		for (size_t j = 0; j < s; ++j)
 		{
 			for (size_t i = 0; i < m; ++i)
 			{
-				const size_t k = L * (2 * m * j + i);
-				const GFp2 w_is = w[i * s];
-
-				for (size_t l = 0; l < L; ++l)
-				{
-					const GFp2 u0 = GFp2(x[2 * (k + 0 * m) + 0 + l], x[2 * (k + 0 * m) + L + l]);
-					const GFp2 u1 = GFp2(x[2 * (k + L * m) + 0 + l], x[2 * (k + L * m) + L + l]).mulconj(w_is);
-					const GFp2 v0 = u0 + u1, v1 = u0 - u1;
-					v0.store(x[2 * (k + 0 * m) + 0 + l], x[2 * (k + 0 * m) + L + l]);
-					v1.store(x[2 * (k + L * m) + 0 + l], x[2 * (k + L * m) + L + l]);
-				}
+				const size_t k = 4 * m * j + 2 * i;
+				bck2(&x[k + 0 * m], &x[k + 2 * m], w[i * s], 1);
 			}
 		}
 	}
+
+	// Radix-4 DIF, one iteration
+	static void forward4(Zp * const x, const GFp2 * const w, const size_t m, const size_t s)
+	{
+		for (size_t j = 0; j < s; ++j)
+		{
+			for (size_t i = 0; i < m; ++i)
+			{
+				const size_t k = 8 * m * j + 2 * i;
+				fwd4(&x[k + 0 * m], &x[k + 2 * m], &x[k + 4 * m], &x[k + 6 * m], w[i * s], w[2 * i * s], w[3 * i * s], 1);
+			}
+		}
+	}
+
+	// Radix-4 DIT, one iteration
+	static void backward4(Zp * const x, const GFp2 * const w, const size_t m, const size_t s)
+	{
+		for (size_t j = 0; j < s; ++j)
+		{
+			for (size_t i = 0; i < m; ++i)
+			{
+				const size_t k = 8 * m * j + 2 * i;
+				bck4(&x[k + 0 * m], &x[k + 2 * m], &x[k + 4 * m], &x[k + 6 * m], w[i * s], w[2 * i * s], w[3 * i * s], 1);
+			}
+		}
+	}
+
+	// Radix-2 DIF, one iteration, each point is a vector of three GFp2
+	static void forward2_3(Zp * const x, const GFp2 * const w, const size_t m, const size_t s)
+	{
+		for (size_t j = 0; j < s; ++j)
+		{
+			for (size_t i = 0; i < m; ++i)
+			{
+				const size_t i_3 = div3(uint32_t(i));
+				const size_t k = 4 * m * j + 3 * i_3 + i;
+				fwd2(&x[k + 0 * m], &x[k + 2 * m], w[i_3 * s], 3);
+			}
+		}
+	}
+
+	// Radix-2 DIT, one iteration, each point is a vector of three GFp2
+	static void backward2_3(Zp * const x, const GFp2 * const w, const size_t m, const size_t s)
+	{
+		for (size_t j = 0; j < s; ++j)
+		{
+			for (size_t i = 0; i < m; ++i)
+			{
+				const size_t i_3 = div3(uint32_t(i));
+				const size_t k = 4 * m * j + 3 * i_3 + i;
+				bck2(&x[k + 0 * m], &x[k + 2 * m], w[i_3 * s], 3);
+			}
+		}
+	}
+
+	// Radix-4 DIF, one iteration, each point is a vector of three GFp2
+	static void forward4_3(Zp * const x, const GFp2 * const w, const size_t m, const size_t s)
+	{
+		for (size_t j = 0; j < s; ++j)
+		{
+			for (size_t i = 0; i < m; ++i)
+			{
+				const size_t i_3 = div3(uint32_t(i));
+				const size_t k = 8 * m * j + 3 * i_3 + i;
+				fwd4(&x[k + 0 * m], &x[k + 2 * m], &x[k + 4 * m], &x[k + 6 * m], w[i_3 * s], w[2 * i_3 * s], w[3 * i_3 * s], 3);
+			}
+		}
+	}
+
+	// Radix-4 DIT, one iteration, each point is a vector of three GFp2
+	static void backward4_3(Zp * const x, const GFp2 * const w, const size_t m, const size_t s)
+	{
+		for (size_t j = 0; j < s; ++j)
+		{
+			for (size_t i = 0; i < m; ++i)
+			{
+				const size_t i_3 = div3(uint32_t(i));
+				const size_t k = 8 * m * j + 3 * i_3 + i;
+				bck4(&x[k + 0 * m], &x[k + 2 * m], &x[k + 4 * m], &x[k + 6 * m], w[i_3 * s], w[2 * i_3 * s], w[3 * i_3 * s], 3);
+			}
+		}
+	}
+
 
 	// Weighted convolution of three numbers
 	static void weighted_sqr3(GFp2 z[3], const GFp2 & w, const GFp2 & wi)
@@ -265,7 +352,7 @@ public:
 	// A "Four Step" NTT algorithm, where n_1 = 2^m and n_2 = 3.
 	// See Bailey, D.H. FFTs in external or hierarchical memory. J Supercomput 4, 23–35 (1990).
 	// Compute the last stage of the length-n/2 transform (combine z[k] and z[-k] with a {n/3}th root).
-	// Multiply by the four-step twiddle factors, calculate the cyclic convolution of length  and apply the reverse operations.
+	// Multiply by the four-step twiddle factors, calculate the cyclic convolution of length three and apply the reverse operations.
 	static void sqr3(Zp * const x, const GFp2 * const w, const size_t n)
 	{
 		static const Zp R6 = Zp::root_nth(6), R6i = R6.invert();
@@ -314,6 +401,50 @@ public:
 				zk.store(x[6 * k + 0 + l], x[6 * k + 3 + l]); zmk.store(x[6 * mk + 0 + l], x[6 * mk + 3 + l]);
 			}
 		}
+	}
+
+public:
+	GFp2 pow(const uint64_t e) const
+	{
+		if (e == 0) return GFp2(Zp(1), Zp(0));
+		GFp2 r = GFp2(Zp(1), Zp(0)), y = *this;
+		for (uint64_t i = e; i != 1; i /= 2) { if (i % 2 != 0) r = r.mul(y); y = y.sqr(); }
+		return r.mul(y);
+	}
+
+	GFp2 invert() const
+	{
+		// 1 / (s0 + i * s1) = (s0 - i * s1) / (s0^2 + s1^2)
+		return conj() * (_s0.sqr() + _s1.sqr()).invert();
+	}
+
+	static const GFp2 root_nth(const size_t n) { return GFp2(Zp(primroot_0), Zp(primroot_1)).pow(primroot_order / n); }
+
+	static void square_2(Zp * const x, const GFp2 * const w, const size_t n)
+	{
+		// for (size_t m = n / 2, s = 1; m >= 2; m /= 2, s *= 2) forward2(x, w, m / 2, s);
+		size_t m = n / 2, s = 1; for (; m >= 4; m /= 4, s *= 4) forward4(x, w, m / 4, s);
+		if (m == 2) forward2(x, w, 1, n / 4);
+
+		sqr(x, &w[n / 2], n);
+
+		// for (size_t m = 2, s = n / 4; s >= 1; m *= 2, s /= 2) backward2(x, w, m / 2, s);
+		if (m == 2) backward2(x, w, 1, n / 4);
+		m *= 4; s /= 4; for (; s >= 1; m *= 4, s /= 4) backward4(x, w, m / 4, s);
+
+	}
+
+	static void square_3(Zp * const x, const GFp2 * const w, const size_t n)
+	{
+		// for (size_t m = n / 2, s = 1; m >= 6; m /= 2, s *= 2) forward2_3(x, w, m / 2, s);
+		size_t m = n / 2, s = 1; for (; m >= 12; m /= 4, s *= 4) forward4_3(x, w, m / 4, s);
+		if (m == 6) forward2_3(x, w, 3, n / 12);
+
+		sqr3(x, &w[n / 6], n);
+
+		// for (size_t m = 6, s = n / 12; s >= 1; m *= 2, s /= 2) backward2_3(x, w, m / 2, s);
+		if (m == 6) backward2_3(x, w, 3, n / 12);
+		m *= 4; s /= 4; for (; s >= 1; m *= 4, s /= 4) backward4_3(x, w, m / 4, s);
 	}
 };
 
@@ -429,36 +560,36 @@ private:
 	static void init_twiddle_factors(GF * const w, const size_t n)
 	{
 		// Radix-2
-		const GF r = (n % 3 != 0) ? GF::root_nth(n / 2) : GF::root_nth(n / 6);
-		for (size_t j = 0; j < n; ++j) w[j] = r.pow(j);
+		const size_t n3 = (n % 3 != 0) ? n : n / 3;
+		const GF r = GF::root_nth(n3 / 2);
+		for (size_t j = 0; j < n3 / 2; ++j) w[j] = r.pow(j);
 
 		// Hermitian product
-		if (n % 3 != 0)
+		GF * const wh = &w[n3 / 2];
+		if (n % 3 != 0)	// n = 2^m
 		{
 			const GF r_n_2 = GF::root_nth(n / 2);
-			for (size_t j = 0; j < n / 4; ++j) w[n + j] = r_n_2.pow(bitrev(j, n / 4));
+			for (size_t j = 0; j < n / 4; ++j) wh[j] = r_n_2.pow(bitrev(j, n / 4));
 		}
-		else
+		else			// n = 3 * 2^m
 		{
 			const GF r_n = GF::root_nth(n), r_n_3 = GF::root_nth(n / 3);
 			for (size_t j = 0; j < n / 12; ++j)
 			{
 				const size_t jr = bitrev(j, n / 12);
-				w[n + 5 * j + 0] = r_n_3.pow(bitrev(j, n / 12));	// Four step factor
+				wh[5 * j + 0] = r_n_3.pow(bitrev(j, n / 12));	// Four step factor
 				const GF rj = r_n.pow(jr);
-				w[n + 5 * j + 1] = rj; w[n + 5 * j + 2] = rj.invert();
+				wh[5 * j + 1] = rj; wh[5 * j + 2] = rj.invert();
 				const GF rjp = r_n.pow(n / 6 - jr);
-				w[n + 5 * j + 3] = rjp; w[n + 5 * j + 4] = rjp.invert();
+				wh[5 * j + 3] = rjp; wh[5 * j + 4] = rjp.invert();
 			}
 		}
 	}
 
 public:
 	mersenne(const uint32_t q) : _n(transform_size(q)),
-		_x61(new Z61[_n]), _x31(new Z31[_n]),
-		_w61(new GF61[2 * _n]), _w31(new GF31[2 * _n]),
-		_w_ib61(new uint8_t[_n]), _w_ib31(new uint8_t[_n]),
-		_digit_width(new uint8_t[_n])
+		_x61(new Z61[_n]), _x31(new Z31[_n]), _w61(new GF61[_n]), _w31(new GF31[_n]),
+		_w_ib61(new uint8_t[_n]), _w_ib31(new uint8_t[_n]), _digit_width(new uint8_t[_n])
 	{
 		const size_t n = _n;
 
@@ -538,36 +669,25 @@ public:
 		const uint8_t * const w_ib31 = _w_ib31;
 		const size_t n = _n;
 
-		if (_n % 3 != 0)
+		// Weighted convolutions
+		if (_n % 3 != 0)	// n = 2^m
 		{
-			// Weighted convolution, radix-2 transforms
-
 			Z61::weight(x61, w_ib61, n);
-			for (size_t m = n / 4, s = 1; m >= 1; m /= 2, s *= 2) GF61::forward2<1>(x61, w61, m, s);
-			GF61::sqr(x61, &w61[n], n);
-			for (size_t m = 1, s = n / 4; s >= 1; m *= 2, s /= 2) GF61::backward2<1>(x61, w61, m, s);
+			GF61::square_2(x61, w61, n);
 			Z61::unweight_norm(x61, w_ib61, n);
 
 			Z31::weight(x31, w_ib31, n);
-			for (size_t m = n / 4, s = 1; m >= 1; m /= 2, s *= 2) GF31::forward2<1>(x31, w31, m, s);
-			GF31::sqr(x31, &w31[n], n);
-			for (size_t m = 1, s = n / 4; s >= 1; m *= 2, s /= 2) GF31::backward2<1>(x31, w31, m, s);
+			GF31::square_2(x31, w31, n);
 			Z31::unweight_norm(x31, w_ib31, n);
 		}
-		else
+		else				// n = 3 * 2^m
 		{
-			// Weighted convolution, radix-2 + radix-3 transforms
-
 			Z61::weight(x61, w_ib61, n);
-			for (size_t m = n / 4 / 3, s = 1; m >= 1; m /= 2, s *= 2) GF61::forward2<3>(x61, w61, m, s);
-			GF61::sqr3(x61, &w61[n], n);
-			for (size_t m = 1, s = n / 4 / 3; s >= 1; m *= 2, s /= 2) GF61::backward2<3>(x61, w61, m, s);
+			GF61::square_3(x61, w61, n);
 			Z61::unweight_norm(x61, w_ib61, n);
 
 			Z31::weight(x31, w_ib31, n);
-			for (size_t m = n / 4 / 3, s = 1; m >= 1; m /= 2, s *= 2) GF31::forward2<3>(x31, w31, m, s);
-			GF31::sqr3(x31, &w31[n], n);
-			for (size_t m = 1, s = n / 4 / 3; s >= 1; m *= 2, s /= 2) GF31::backward2<3>(x31, w31, m, s);
+			GF31::square_3(x31, w31, n);
 			Z31::unweight_norm(x31, w_ib31, n);
 		}
 
